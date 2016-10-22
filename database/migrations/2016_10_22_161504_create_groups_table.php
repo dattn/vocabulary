d19aa28d11b71909f8fb332f516b6ea02ad5d4c1
Daniel Duton <daniel@duton.lu>
2016-10-22 18:16:08 +0200

    add groups table

---
+++ database/migrations/2016_10_22_161504_create_groups_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('label');
+            $table->integer('level_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('level_id')
+                  ->references('id')->on('levels')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
