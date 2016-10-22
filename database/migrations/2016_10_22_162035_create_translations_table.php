3dd0173b34874d2d9d99d117a9422261cfd7a587
Daniel Duton <daniel@duton.lu>
2016-10-22 18:23:32 +0200

    add translations table

---
+++ database/migrations/2016_10_22_162035_create_translations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('from');
+            $table->string('to');
+            $table->integer('group_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('group_id')
+                  ->references('id')->on('groups')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
